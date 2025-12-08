7fee3aadf7959edac75bc09e718ed1230ec3d4ec
Cristobal Adán Ávlia Vences <caav@SPSMXTC72LTOF25.local>
2025-12-08 17:02:36 -0600

    Se agrega el ms de sprinBootAdmin

---
+++ springbootadmin/src/main/java/com/paymentchain/springbootadmin/SpringbootadminApplication.java
@@ -0,0 +1,20 @@
+package com.paymentchain.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringbootadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootadminApplication.class, args);
+	}
+
+}
